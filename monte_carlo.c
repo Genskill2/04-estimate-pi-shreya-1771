1265c99fcb36004ae57c446b80f94e109ae48cc2
shreya-1771 <84712068+shreya-1771@users.noreply.github.com>
2021-06-02 16:00:49 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -14,14 +14,14 @@ float frandom() {
 float mc_pi(int x){
   float c=0.0;
   
-  for(int i=0;i<=x;i++){
+  for(int i=0;i<x;i++){
   float x= frandom();
   float y=frandom();
   float d=sqrt((x*x)+(y*y));
     
     if(d<=1){
       c++;
-    }
+    } }
     float ans= 4.0*(c/(float)x);
     return (ans);
   }
